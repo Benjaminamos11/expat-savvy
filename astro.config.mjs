c4a45f6929064afac594abe76da29b29840758bf
bw <bw@expat-savvy.ch>
2025-04-10 19:09:17 +0000

    add netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import { onBuildComplete } from './src/scripts/build-hooks.js';
 
 export default defineConfig({
   integrations: [
@@ -14,19 +13,14 @@ export default defineConfig({
   ],
   site: 'https://expat-savvy.ch',
   compressHTML: true,
-  build: {
-    inlineStylesheets: 'always'
-  },
   vite: {
     build: {
       cssCodeSplit: false,
       assetsInlineLimit: 4096,
+      inlineStylesheets: 'always',
     },
     ssr: {
-      noExternal: ['@fontsource/inter']
+        noExternal: ['@fontsource/inter']
     }
-  },
-  hooks: {
-    'astro:build:done': onBuildComplete
   }
 });
